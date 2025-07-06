94bbd7cfffa20c5a3b2e73b1e9a9a660a372cb14
samkumo <samuli.kumo@gmail.com>
2025-07-06 13:36:47 +0200

    Step 7: JSONModel

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,30 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], (Controller, MessageToast) => {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], (Controller, MessageToast, JSONModel) => {
     "use strict";
 
+    //This returns Controller-object which we have extended with new functions
     return Controller.extend("ui5.walkthrough.controller.App", {
+        //Here we define the functions and implement the functions
+        //Different from ABAP where definition and implementation
+        //are clearly separated.
+
+        onInit() {
+            //Set the data model for the view using this controller
+
+            //Define object to describe the data
+            const oData = {
+                recipient: {
+                    name: "World"
+                }
+            };
+            //Initialize JSOMModel of this data
+            const oModel = new JSONModel(oData);
+            //Set the model for current view
+            this.getView().setModel(oModel);
+        },
         onShowHello() {
             MessageToast.show("Hello UI5!")
         }
