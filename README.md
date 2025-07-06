5b2cce377f59df58d4aad6e37d8d846dce3cdd67
samkumo <samuli.kumo@gmail.com>
2025-07-06 14:08:33 +0200

    Step 8: Translatable texts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 SAPUI5 Demo Walkthrough
+
 Source: https://sapui5.hana.ondemand.com/#/topic/3da5f4be63264db99f2e5b04c5e853db
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-], (Controller, MessageToast, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], (Controller, MessageToast, JSONModel, ResourceModel) => {
     "use strict";
 
     //This returns Controller-object which we have extended with new functions
@@ -24,9 +25,21 @@ sap.ui.define([
             const oModel = new JSONModel(oData);
             //Set the model for current view
             this.getView().setModel(oModel);
+
+            //Set i18n model which contains translated texts
+            const i18nModel = new ResourceModel({
+                bundleName: "ui5.walkthrough.i18n.i18n"
+            });
+            //Set the i18n model to this view and give it an alias
+            this.getView().setModel(i18nModel, "i18n");
         },
         onShowHello() {
-            MessageToast.show("Hello UI5!")
+            //Read message text from i18n model
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sRecipient = this.getView().getModel().getProperty("/recipient/name"); //s = string
+            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+            MessageToast.show(sMsg)
         }
     }
     );
