437aa3a129cd30dd4bc1c7090ba3e4453b70722e
samkumo <samuli.kumo@gmail.com>
2025-07-06 15:12:12 +0200

    Step 9: Component configuration

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -12,27 +12,6 @@ sap.ui.define([
         //Different from ABAP where definition and implementation
         //are clearly separated.
 
-        onInit() {
-            //Set the data model for the view using this controller
-
-            //Define object to describe the data
-            const oData = {
-                recipient: {
-                    name: "World"
-                }
-            };
-            //Initialize JSOMModel of this data
-            const oModel = new JSONModel(oData);
-            //Set the model for current view
-            this.getView().setModel(oModel);
-
-            //Set i18n model which contains translated texts
-            const i18nModel = new ResourceModel({
-                bundleName: "ui5.walkthrough.i18n.i18n"
-            });
-            //Set the i18n model to this view and give it an alias
-            this.getView().setModel(i18nModel, "i18n");
-        },
         onShowHello() {
             //Read message text from i18n model
             const oBundle = this.getView().getModel("i18n").getResourceBundle();
